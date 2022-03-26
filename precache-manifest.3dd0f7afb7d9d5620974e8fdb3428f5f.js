c0dc8dc4c4f3069f14d27eb85ab2ca4ebe1abcb8
NikhilKDhande <100127292+NikhilKDhande@users.noreply.github.com>
2022-03-26 13:21:21 +0530

    Updates

---
+++ precache-manifest.3dd0f7afb7d9d5620974e8fdb3428f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a6f846c125e71cc0d569b977ab1ac8",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "8b2e1a7bd6294025f30b",
+    "url": "/random-quote-generator/static/css/main.6e2f2ec3.chunk.css"
+  },
+  {
+    "revision": "bbe1b8e7e1c45e808cc5",
+    "url": "/random-quote-generator/static/js/2.20349545.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.20349545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2e1a7bd6294025f30b",
+    "url": "/random-quote-generator/static/js/main.0f088dd3.chunk.js"
+  },
+  {
+    "revision": "9ed2fba8b3eb66c1aedf",
+    "url": "/random-quote-generator/static/js/runtime-main.08bf99a4.js"
+  }
+]);
